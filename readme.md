fe57bc491dc5b86ddf7151e253fd271e8ba0f630
Bryan Braun <bbraun7@gmail.com>
2021-03-23 23:22:44 -0400

    third time's a charm

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Infinitely Nested iFrames
+
+Attempting to infinitely nest iFrames for fun.
+
+## To run it locally:
+
+1. In the terminal, run your server: `./start.sh` (requires docker)
+2. Browse to: `localhost`
---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker kill infinitely-nested-iframes
+docker rm infinitely-nested-iframes
+
+docker run --name infinitely-nested-iframes -d \
+  -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
+  -v $PWD/static:/etc/nginx/html \
+  -p 80:80 \
+  nginx
