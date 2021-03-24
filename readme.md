e8fa7c338d9e8c76349f75a6265648b079532fba
Bryan Braun <bbraun7@gmail.com>
2021-03-23 23:48:32 -0400

    feat: reorganize files for hosting on github pages

--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 Attempting to infinitely nest iFrames for fun.
 
+**[See the demo here](https://bryanbraun.com/infinitely-nested-iframes).**
+
 ## To run it locally:
 
 1. In the terminal, run your server: `./start.sh` (requires docker)
--- start.sh
+++ start.sh
@@ -2,9 +2,8 @@
 
 docker kill infinitely-nested-iframes
 docker rm infinitely-nested-iframes
-
 docker run --name infinitely-nested-iframes -d \
   -v $PWD/nginx.conf:/etc/nginx/nginx.conf \
-  -v $PWD/static:/etc/nginx/html \
+  -v $PWD:/etc/nginx/html \
   -p 80:80 \
   nginx
